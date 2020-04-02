b8d894c789659e0744c2dcb9ab95c651dd6101ec
lucasokazava <lucasokazava@gmail.com>
2020-04-02 04:49:19 -0300

    add exercise_1.js

---
+++ exercises/4_1/exercise_1.js
@@ -0,0 +1,12 @@
+/*Faça cinco programas, um para cada operação aritmética básica.
+Seu programa deve ter duas variáveis, a e b, definidas no começo
+com os valores que serão operados.*/
+
+let a = 24;
+let b = 7;
+
+console.log("A soma de a + b é igual a " + (a + b) + ".");
+console.log("A subtração de a - b é igual a " + (a - b) + ".");
+console.log("A multiplicação de a * b é igual a " + (a * b) + ".");
+console.log("A divisão de a / b é igual a " + (a / b) + ".");
+console.log("O módulo de a % b é igual a " + (a % b) + ".");
