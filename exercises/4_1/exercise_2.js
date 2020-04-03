9c249b132d9771d085369b8c0408c9ef6eebccf3
lucasokazava <lucasokazava@gmail.com>
2020-04-03 01:14:49 -0300

    add exercise_2.js

---
+++ exercises/4_1/exercise_2.js
@@ -0,0 +1,15 @@
+/*Faça um programa que retorne o maior de dois números.
+Defina no começo do programa duas variáveis com os valores que serão comparados.*/
+
+let a = 7;
+let b = 12;
+
+if (a > b) {
+    console.log(a);
+}
+else if (a < b) {
+    console.log(b);
+}
+else {
+    console.log("Os dois números tem o mesmo valor!");
+}
