e2daceafcdd4f1ade67e64a75d31c919321c7283
lucasokazava <lucasokazava@gmail.com>
2020-05-17 17:41:09 -0300

    add exercise_9.js

---
+++ exercises/4_1/exercise_9.js
@@ -0,0 +1,14 @@
+/*Escreva um programa que defina três números em variáveis no seu começo e retorne true se uma das três for ímpar. Caso contrário, ele retorna false.
+Use somente um if.*/
+
+let a = 8;
+let b = 10;
+let c = 1;
+resultado = false;
+
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+    resultado = true;
+}
+
+console.log(resultado);
+
